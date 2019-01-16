de775f983cbedea67e9925f1dd103020ff334fac
Delagardi <delagardi@ymail.com>
2019-01-16 23:46:34 +0200

    Added action icons

--- src/components/pages/item-list/item-list.js
+++ src/components/pages/item-list/item-list.js
@@ -5,6 +5,7 @@ import './item-list.css';
 const ItemList = () => {
   return (
     <div className="container">
+      <h3 className="order-list-header">Your order</h3>
       <table className="order-list">
         <thead>
           <tr>
@@ -21,14 +22,22 @@ const ItemList = () => {
             <td>Sapiens: A Brief History of Humankind</td>
             <td>1</td>
             <td>$34</td>
-            <td>+</td>
+            <td>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-remove-circle-outline"></i></a>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-add-circle-outline"></i></a>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-close-circle-outline"></i></a>
+            </td>
           </tr>
           <tr>
             <td>2</td>
             <td>The Hero with a Thousand Faces</td>
             <td>1</td>
             <td>$43</td>
-            <td>-</td>
+            <td>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-remove-circle-outline"></i></a>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-add-circle-outline"></i></a>
+              <a className="order-item-link d-block" href="remove"><i className="icon d-inline-block ion-md-close-circle-outline"></i></a>
+            </td>
           </tr>
         </tbody>
       </table>
