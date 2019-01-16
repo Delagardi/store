da365a365dc928ccba8a7cec047205eb5a4b5500
Delagardi <delagardi@ymail.com>
2019-01-16 03:57:46 +0200

    Added frontpage

--- src/components/pages/frontpage/frontpage.js
+++ src/components/pages/frontpage/frontpage.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import './frontpage.css';
+import book1 from '../../../upload/book-preview/sapiens.jpg';
+import book2 from '../../../upload/book-preview/hero faces.jpg';
+
 const Frontpage = () => {
-  return <h2>This is Front page</h2>
+  return(
+    <div className="container">
+      <div className="item d-flex">
+        <div className="item-img">
+          <img height="200" src={book1} alt="Sapiens: A Brief History of Humankind" />
+        </div>
+        <div className="item-data">
+          <h3>Sapiens: A Brief History of Humankind</h3>
+          <span>Yuval Noah Harari</span>
+          <span className="item-price d-block">$34</span>
+        </div>
+      </div>
+      <div className="item d-flex">
+        <div className="item-img">
+          <img height="200" src={book2} alt="The Hero with a Thousand Faces " />
+        </div>
+        <div className="item-data">
+          <h3>The Hero with a Thousand Faces </h3>
+          <span>Joseph Campbell</span>
+          <span className="item-price d-block">$43</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Frontpage;
