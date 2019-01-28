a872aa1334080e2740d4c14a8f6eda370df9e869
Delagardi <delagardi@ymail.com>
2019-01-28 23:05:19 +0200

    Added redux extension:

--- src/components/pages/frontpage/frontpage.js
+++ src/components/pages/frontpage/frontpage.js
@@ -4,23 +4,40 @@ import FrontpageItem from '../frontpage-item';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../../../actions';
+
+import ServiceBookstore from '../../../services/service-bookstore';
+
 import withBookstoreService from '../../hoc';
 
 
 import './frontpage.css';
 
+const serviceBookstore = new ServiceBookstore();
+
 class Frontpage extends Component {
   componentDidMount() {
-    const { bookstoreService, onAddToCart, booksData } = this.props;
-    console.log(bookstoreService);
+    const { bookstoreService, onAddToCart} = this.props;
+
     const data = bookstoreService.getBooks();
+
+    // this.serviceBookstore
+    //   .getBooks()
+    //   .then( (booksData) => {
+    //     this.setState({
+    //       booksData: booksData
+    //     });
+    // });
 
     console.log('data:');
     console.log(data);
   }
   
   render() {
-    const { onAddToCart, booksData } = this.props;
+    const { onAddToCart } = this.props;
+    const { booksData } = this.state;
+
+    console.log('booksData:');
+    console.log(booksData);
     
     return (
       booksData.map( (item) => {
